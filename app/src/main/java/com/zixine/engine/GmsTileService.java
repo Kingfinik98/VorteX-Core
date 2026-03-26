4780422942a11239023a4abac388516ce647f2f1
waheiiiddd-lab <waheiiiddd@gmail.com>
2026-03-27 04:03:04 +0700

    Refactor GmsTileService to target specific packages

--- app/src/main/java/com/zixine/engine/GmsTileService.java
+++ app/src/main/java/com/zixine/engine/GmsTileService.java
@@ -8,15 +8,13 @@ public class GmsTileService extends TileService {
     public void onClick() {
         Tile t = getQsTile();
         boolean active = (t.getState() == Tile.STATE_INACTIVE);
-        // LIST GOOGLE MANUAL
-        String gms = "com.google.android.gms com.android.vending com.google.android.gsf com.google.android.projection.gearhead com.google.android.tts";
-        
+        String target = "com.google.android.gms com.android.vending com.google.android.gsf";
         if (active) {
-            exec("pm suspend --user 0 " + gms + "; am force-stop com.google.android.gms;");
+            exec("pm suspend --user 0 " + target + "; am force-stop com.google.android.gms;");
             t.setState(Tile.STATE_ACTIVE);
             Toast.makeText(this, "GMS: KILLED 💀", 0).show();
         } else {
-            exec("pm unsuspend --user 0 " + gms + ";");
+            exec("pm unsuspend --user 0 " + target + ";");
             t.setState(Tile.STATE_INACTIVE);
             Toast.makeText(this, "GMS: RESTORED 🌍", 0).show();
         }
